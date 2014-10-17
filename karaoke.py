ba8c670504f53c479f93ce19a68150ea45a8527f
CarlosJLoH <carlos_7__14@hotmail.com>
2014-10-17 23:26:15 +0200

    kara

--- karaoke.py
+++ karaoke.py
@@ -1,8 +1,34 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 import sys
+import os
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+from smallsmilhandler import SmallSMILHandler
 
-from KaraokeLocal import KaraokeLocal
+class KaraokeLocal():
+
+	def __init__ (self, fichero):
+		self.parser = make_parser()
+		self.sHandler = SmallSMILHandler()
+		self.parser.setContentHandler(self.sHandler)
+		self.parser.parse(open(fichero))
+
+	def __str__ (self):
+		etiquetas = self.sHandler.get_tags()
+		for i in etiquetas:
+			print i.toString()
+		print "\n"
+
+	def __do_local__ (self):
+		etiquetas = self.sHandler.get_tags()
+		for i in etiquetas:
+			print i.toString()
+			if i.toString().find("http")>=0:
+				src = i.toString().split("\t")[1]
+				path = src.replace("src=", "")
+				print "... Downloading " + path.split("/")[-1] + "..."
+				os.system("wget -q " + path)
 
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
