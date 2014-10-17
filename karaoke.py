cb780d4115e41ceee8a7b00d9b70b769a61617b4
CarlosJLoH <carlos_7__14@hotmail.com>
2014-10-18 01:55:30 +0200

    pep8

--- karaoke.py
+++ karaoke.py
@@ -6,34 +6,35 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 from smallsmilhandler import SmallSMILHandler
 
+
 class KaraokeLocal():
 
-	def __init__ (self, fichero):
-		self.parser = make_parser()
-		self.sHandler = SmallSMILHandler()
-		self.parser.setContentHandler(self.sHandler)
-		self.parser.parse(open(fichero))
+    def __init__(self, fichero):
+        self.parser = make_parser()
+        self.sHandler = SmallSMILHandler()
+        self.parser.setContentHandler(self.sHandler)
+        self.parser.parse(open(fichero))
 
-	def __str__ (self):
-		etiquetas = self.sHandler.get_tags()
-		for i in etiquetas:
-			print i.toString()
-		print "\n"
+    def __str__(self):
+        etiquetas = self.sHandler.get_tags()
+        for i in etiquetas:
+            print i.toString()
+        print "\n"
 
-	def __do_local__ (self):
-		etiquetas = self.sHandler.get_tags()
-		for i in etiquetas:
-			print i.toString()
-			if i.toString().find("http")>=0:
-				src = i.toString().split("\t")[1]
-				path = src.replace("src=", "")
-				print "... Downloading " + path.split("/")[-1] + "..."
-				os.system("wget -q " + path)
+    def __do_local__(self):
+        etiquetas = self.sHandler.get_tags()
+        for i in etiquetas:
+            print i.toString()
+            if i.toString().find("http") >= 0:
+                src = i.toString().split("\t")[1]
+                path = src.replace("src=", "")
+                print "... Downloading " + path.split("/")[-1] + "..."
+                os.system("wget -q " + path)
 
 if __name__ == "__main__":
-	if len(sys.argv) != 2:
-		print "Usage: python karaoke.py file.smil"
-	else:
-		kLocal = KaraokeLocal(sys.argv[1])
-    	kLocal.__str__()
-    	kLocal.__do_local__()
+    if len(sys.argv) != 2:
+        print "Usage: python karaoke.py file.smil"
+    else:
+        kLocal = KaraokeLocal(sys.argv[1])
+    kLocal.__str__()
+    kLocal.__do_local__()
