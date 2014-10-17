3f1a28caa599a88cd393ef2cf6d67a6d626525b5
CarlosJLoH <carlos_7__14@hotmail.com>
2014-10-17 12:53:18 +0200

    ej6

--- karaoke.py
+++ karaoke.py
@@ -1,25 +1,13 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 import sys
-import os
-from xml.sax import make_parser
-from xml.sax.handler import ContentHandler
-from smallsmilhandler import SmallSMILHandler
+
+from KaraokeLocal import KaraokeLocal
 
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
 		print "Usage: python karaoke.py file.smil"
 	else:
-		parser = make_parser()
-    	sHandler = SmallSMILHandler()
-    	parser.setContentHandler(sHandler)
-    	parser.parse(open('karaoke.smil'))
-    	etiquetas = sHandler.get_tags()
-    	for i in etiquetas:
-    		print i.toString()
-    		if i.toString().find("http")>=0:
-				src = i.toString().split("\t")[1]
-				path = src.replace("src=", "")
-				print "... Downloading " + path.split("/")[-1] + "..."
-				os.system("wget -q " + path)
-
+		kLocal = KaraokeLocal(sys.argv[1])
+    	kLocal.__str__()
+    	kLocal.__do_local__()
